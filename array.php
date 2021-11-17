241e8285bc73ad3a0cf6a56f624d99c133b0a154
umar4166 <umar@devinsta.com>
2021-11-17 17:04:56 +0500

    PHP Crud

---
+++ array.php
@@ -0,0 +1,20 @@
+<?php
+// DataBase Connection.
+$local='localhost';
+$username='root';
+$password='';
+$db='array';
+$conn=mysqli_connect($local,$username,$password,$db);
+if(!$conn){
+    echo 'You Are Not Connected To Database'. mysqli_connect_error();
+}
+// Connection Has Been Established With Database Name "array"
+
+$names=array("Umar","Asfand","Haris","Hassam");
+$value=array("username"=>"ahmad","name"=>"AliHassan","position"=>"WPDeveloper");
+$value=array("username"=>"ahmad","name"=>"AliHassan","position"=>"WPDeveloper");
+$value=array("username"=>"ahmad","name"=>"AliHassan","position"=>"WPDeveloper");
+/*
+This is my first Project.
+*/
+?>
